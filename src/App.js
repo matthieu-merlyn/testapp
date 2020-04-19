63b41969f8c1d75fbf62a4370f8f3bd989422469
Matthieu Merlyn <matthieu.merlyn@kbc.be>
2020-04-19 14:58:41 +0200

    new test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployment at 19/04 14:50.
+          Deployment at 19/04 14:58.
         </p>
         <a
           className="App-link"
